deb488cd979ef32e1ca376e5d4d9d62137bd37a9
Dan Dowd <danieldowd89@gmail.com>
2025-04-28 21:33:05 -0600

    adds fast csv

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { S3Client } from "@aws-sdk/client-s3";
-import csv from "csv-parser";
+import { parse } from "fast-csv";
 import Busboy from "busboy";
 import { PassThrough } from "stream";
 import { randomUUID } from "crypto";
@@ -10,26 +10,47 @@ const client = new S3Client();
 
 const app = express();
 
+app.use((req, res, next) => {
+  const start = Date.now();
+  console.log("request starting");
+
+  res.on("finish", () => {
+    const duration = Date.now() - start;
+    console.log(`${req.method} ${req.originalUrl} took ${duration}ms`);
+  });
+
+  next();
+});
+
 app.post("/upload", (req, res) => {
+  console.log("upload start");
   const busboy = Busboy({ headers: req.headers });
   const passThrough = new PassThrough();
 
   let s3Promise;
 
   busboy.on("file", (fieldname, fileStream, filename, encoding, mimetype) => {
+    console.log("on file");
+    let count = 1;
     s3Promise = new Upload({
       client,
       params: {
         Bucket: "random-test-bucket-dan",
-        Key: randomUUID(),
+        Key: `test-${randomUUID()}`,
         Body: passThrough,
       },
     }).done();
 
     fileStream
-      .pipe(csv())
+      .pipe(parse())
       .on("data", (row) => {
-        console.log(row);
+        if (count === 1) {
+          console.log("starting data");
+        }
+        if (count % 10_000 === 0) {
+          console.log(row);
+        }
+        count++;
         const csvRow = Object.values(row).join(",") + "\n";
         passThrough.write(csvRow);
       })
